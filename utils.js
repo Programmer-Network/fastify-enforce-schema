844a79e37674315ed270acf1d971c2245a2d4c70
Aleksandar Grbic <hi@aleksandar.xyz>
2022-11-22 23:52:24 +0100

    Added "hasProperties" utility function

--- utils.js
+++ utils.js
@@ -1,3 +1,7 @@
 exports.getErrrorMessage = (schemaType, path) => {
-  return `${path} is missing a ${schemaType} schema`
-}
+  return `${path} is missing a ${schemaType} schema`;
+};
+
+exports.hasProperties = (routeOptions, name) => {
+  return !!Object.keys(routeOptions?.schema?.[name]?.properties || []).length;
+};
