40a58f086971befd72a7021ef5876c1aee598aa8
Aleksandar Grbic <hi@aleksandar.xyz>
2022-12-02 23:00:02 +0100

    Fixed wrong response schema validation

--- utils.js
+++ utils.js
@@ -3,5 +3,26 @@ exports.getErrrorMessage = (schemaType, path) => {
 };
 
 exports.hasProperties = (routeOptions, name) => {
+  if (name === "response") {
+    const schema = Object.keys(routeOptions?.schema?.[name]);
+
+    if(!schema.length) {
+      throw new Error(`No HTTP status codes provided in the response schema`)
+    }
+
+    if(schema.forEach(value) => {
+      if(!Number.isInteger(value)) {
+        throw new Error(`${value} is not a number. HTTP status codes from 100 - 599 supported`)
+      }
+
+      if(value < 100 || value > 599) {
+        throw new Error(`HTTP status codes from 100 - 599 supported`)
+      }
+    })
+
+    return true;
+  }
+
   return !!Object.keys(routeOptions?.schema?.[name]?.properties || []).length;
 };
+
